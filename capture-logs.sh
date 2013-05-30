b7d776a195f7de783d7cbf0a5e955e058556c081
Mike Lundy <mike@fluffypenguin.org>
2013-05-30 13:37:52 -0700

    report test failures to pivotal

--- capture-logs.sh
+++ capture-logs.sh
@@ -4,7 +4,7 @@ set -x
 set -e
 set -o pipefail
 
-if [ -z "$FILESERVER" -o -z "$FS_SSH_USER" -o -z "$RELEASE_VERSION" -o -z "$GITHUB_OWNER" -o -z "$GITHUB_BRANCH" -o -z "$BUILD_NUMBER" -o -z "$WORKSPACE" ]
+if [ -z "$FILESERVER" -o -z "$FS_SSH_USER" -o -z "$RELEASE_VERSION" -o -z "$GITHUB_OWNER" -o -z "$GITHUB_BRANCH" -o -z "$BUILD_NUMBER" -o -z "$WORKSPACE" -o -z "$PIVOTAL_TOKEN" -o -z "$PIVOTAL_PROJECT_ID" ]
 then
     echo "Required variable not set" >&2
     exit 1
@@ -42,4 +42,23 @@ http://$FILESERVER/builds/$GITHUB_OWNER/$GITHUB_BRANCH/debug/functional-test-$RE
 scp $FILESERVER:/home/shared/builds/$GITHUB_OWNER/$GITHUB_BRANCH/debug/functional-test-$RELEASE_VERSION-$BUILD_NUMBER.log.gz .
 EOF
 
+if [ "$GITHUB_OWNER" != "piston" ]; then
+    exit 0
+fi
+
+if ! [ "$TEST_GITHUB_OWNER" = "piston" -o -z "$TEST_GITHUB_OWNER" ]; then
+    exit 0
+fi
+
+read -r -d '' xml <<-EOF || true
+<story>
+    <story_type>bug</story_type>
+    <name>Test Failure: $RELEASE_VERSION</name>
+    <description>https://albino.pistoncloud.com/job/Functional_Tests/$BUILD_NUMBER/console
+http://$FILESERVER/builds/$GITHUB_OWNER/$GITHUB_BRANCH/debug/functional-test-$RELEASE_VERSION-$BUILD_NUMBER.log
+http://$FILESERVER/builds/$GITHUB_OWNER/$GITHUB_BRANCH/debug/functional-test-$RELEASE_VERSION-$BUILD_NUMBER.log.gz</description>
+</story>
 EOF
+
+curl --retry 5 --retry-delay 5 -H "X-TrackerToken: $PIVOTAL_TOKEN" -X POST -H "Content-type: application/xml" \
+    -d "$xml" http://www.pivotaltracker.com/services/v3/projects/$PIVOTAL_PROJECT_ID/stories
