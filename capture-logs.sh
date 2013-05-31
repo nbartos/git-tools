79eec81a7a201cf8629dea6a10dd095877bb8072
Mike Lundy <mike@fluffypenguin.org>
2013-05-31 12:46:04 -0700

    pull file-bug out of capture-logs

--- capture-logs.sh
+++ capture-logs.sh
@@ -42,26 +42,3 @@ http://$FILESERVER/builds/$GITHUB_OWNER/$GITHUB_BRANCH/debug/functional-test-$RE
 scp $FILESERVER:/home/shared/builds/$GITHUB_OWNER/$GITHUB_BRANCH/debug/functional-test-$RELEASE_VERSION-$BUILD_NUMBER.log.gz .
 
 EOF
-
-if [ "$GITHUB_OWNER" != "piston" ]; then
-    exit 0
-fi
-
-if ! [ "$TEST_GITHUB_OWNER" = "piston" -o -z "$TEST_GITHUB_OWNER" ]; then
-    exit 0
-fi
-
-read -r -d '' xml <<-EOF || true
-<story>
-    <story_type>bug</story_type>
-    <name>Test Failure: $RELEASE_VERSION [automatically filed by Jenkins]</name>
-    <labels>jenkins</labels>
-    <description>https://albino.pistoncloud.com/job/Functional_Tests/$BUILD_NUMBER/console
-http://$FILESERVER/builds/$GITHUB_OWNER/$GITHUB_BRANCH/debug/functional-test-$RELEASE_VERSION-$BUILD_NUMBER.log
-http://$FILESERVER/builds/$GITHUB_OWNER/$GITHUB_BRANCH/debug/functional-test-$RELEASE_VERSION-$BUILD_NUMBER.log.gz</description>
-</story>
-EOF
-
-echo "Filing Pivotal bug for $RELEASE_VERSION"
-curl --retry 5 --retry-delay 5 -H "X-TrackerToken: $PIVOTAL_TOKEN" -X POST -H "Content-type: application/xml" \
-    -d "$xml" http://www.pivotaltracker.com/services/v3/projects/$PIVOTAL_PROJECT_ID/stories
