445a42ef40d22dcd6b7c8627022845358ac1e193
Mike Lundy <mike@fluffypenguin.org>
2013-08-25 12:51:47 -0700

    put the test build id in the title

--- file-bug.sh
+++ file-bug.sh
@@ -21,7 +21,7 @@ fi
 read -r -d '' xml <<-EOF || true
 <story>
     <story_type>bug</story_type>
-    <name>Test Failure: $RELEASE_VERSION [automatically filed by Jenkins]</name>
+    <name>Test Failure: $RELEASE_VERSION [$TEST_BUILD_NUMBER] [automatically filed by Jenkins]</name>
     <labels>jenkins</labels>
     <description>https://albino.pistoncloud.com/job/Functional_Tests/$TEST_BUILD_NUMBER/console
 http://$FILESERVER/builds/$GITHUB_OWNER/$GITHUB_BRANCH/debug/functional-test-$RELEASE_VERSION-$TEST_BUILD_NUMBER.log
